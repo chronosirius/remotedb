6a5d5ae7bc0a66434c8d4e190a520d269d4c3b25
chronosirius <73508925+chronosirius@users.noreply.github.com>
2023-04-01 10:36:55 -0700

    Create main.py
    created flask app for update management

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  with open('save.json') as f:
+    return f.read(), 200
+
+@app.route('/update', methods=['POST'])
+def update():
+  with open('save.json', 'w') as f:
+    f.write(request.form['new'])
+  return "", 200
+  
+app.run('0.0.0.0', int(environ.get('PORT', 80))
